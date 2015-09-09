4bfd573b5e382209e64d55259d1f77792df730a0
Matt Robenolt <matt@ydekproductions.com>
2015-09-09 13:04:08 -0700

    Change repo and maintainer

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -3,9 +3,9 @@ set -e
 
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
-url='git://github.com/docker-library/sentry'
+url='git://github.com/getsentry/docker-sentry'
 
-echo '# maintainer: InfoSiftr <github@infosiftr.com> (@infosiftr)'
+echo '# maintainer: Matt Robenolt <matt@getsentry.com> (@mattrobenolt)'
 
 commit="$(git log -1 --format='format:%H' -- Dockerfile $(awk 'toupper($1) == "COPY" { for (i = 2; i < NF; i++) { print $i } }' Dockerfile))"
 fullVersion="$(grep -m1 'ENV SENTRY_VERSION ' Dockerfile | cut -d' ' -f3)"
